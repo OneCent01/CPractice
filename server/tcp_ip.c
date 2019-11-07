8a20e4cbe638bc891cbd8b9575355f48c9e71b6d
Jacob Penney <jacobpenney@KS190203.local>
2019-11-07 12:28:07 -0800

    tcp/ip server

--- server/tcp_ip.c
+++ server/tcp_ip.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 #include<sys/socket.h> 
-
+#include<unistd.h>
 
 int main()
 {
 	int socket_connection = socket(AF_INET, SOCK_STREAM, 0);
+	int live_conn, len, valread;
+	char buffer[1024] = {0}, *message="Hello, client";
 
 	if(socket_connection == -1) {
 		printf("FAIL: Socket creation\n");
@@ -15,18 +17,52 @@ int main()
 
 	printf("SUCCESS: Socket creation\n");
 	
-	struct sockaddr_in server = {0};
+	struct sockaddr_in server;
 	
-	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
 	server.sin_family = AF_INET;
 	server.sin_port = htons( 8090 );
 	
-	if(connect(socket_connection, (struct sockaddr *)&server, sizeof(server)) < 0) {
+	if(bind(socket_connection, (struct sockaddr *)&server, sizeof(server)) < 0) {
 		printf("FAIL: Socket connection\n");
 		return 1;
 	} 
 	
 	printf("SUCCESS: Socket connection\n");
+
+
+	if((listen(socket_connection, 5)) != 0) {
+		printf("FAIL: Socket listening :(\n");
+		return 1;
+	}
+
+	printf("SUCCESS: Socket listening...\n");
+
+	len = sizeof(server);
+
+	live_conn = accept(socket_connection, (struct sockaddr *)&server, (socklen_t*)&len);
+
+	if(live_conn < 0) {
+		printf("FAIL: Socket accepting connection :(\n");
+		return 1;
+	}
+
+	printf("SUCCESS: Socket accepting connections!\n");
+
+	valread = read(live_conn, buffer, 1024);
+	printf("Incoming request: %s", buffer);
+	printf("\n");
+
+	send(live_conn, message, sizeof(message), 0);
+
+	printf("Message sent. Closing socket...");
+	printf("\n");
+
+
+	close(live_conn);
+
+	printf("SOCKET CLOSED.");
+	printf("\n");
 
 	return 0;
 }
